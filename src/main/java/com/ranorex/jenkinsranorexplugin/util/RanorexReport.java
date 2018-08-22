2a7374d76961d4717e9eeacd3c101bbd452dd4ac
Markus Stögerer <markus.stoegerer@gmail.com>
2018-08-22 14:03:27 +0200

    Create RanorexReport class

---
+++ src/main/java/com/ranorex/jenkinsranorexplugin/util/RanorexReport.java
@@ -0,0 +1,11 @@
+package com.ranorex.jenkinsranorexplugin.util;
+
+public class RanorexReport {
+    private static String ReportDirectory;
+    private static String ReportName;
+    private static String ReportExtension;
+    private static Boolean JunitReport;
+    private static Boolean CompressedReport;
+    private static String CompressedReportDirectory;
+    private static String CompressedReportName;
+}
